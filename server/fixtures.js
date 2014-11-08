401dc2eae5588a64263bbde706220e5badeea3af
Ria Carmin <akimalunar@gmail.com>
2014-11-08 12:23:05 -0800

    Static map is blinded to the db's center

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,23 +1,9 @@
 Meteor.startup(function () {
   if (Places.find().count() === 0) {
     Places.insert({
-      name: 'Place X',
-	    address: 'San Francisco Ferry Building, San Francisco, CA, United States'
-    });
-
-    Places.insert({
-      name: 'Place 2',
-	    address: 'San Francisco Ferry Building, San Francisco, CA, United States'
-    });
-
-    Places.insert({
-      name: 'Place 3',
-	    address: 'San Francisco Ferry Building, San Francisco, CA, United States'
-    });
-
-    Places.insert({
-      name: 'Place 4',
-	    address: 'San Francisco Ferry Building, San Francisco, CA, United States'
+      name: 'Boba Guys',
+	  address: '3491 19th St San Francisco, CA 94110',
+	  center: '37.760732,-122.421202'
     });
   }
 });

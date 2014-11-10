82c7f8b5a78400e14ffc1c01744b069bf9112e61
Ria Carmin <akimalunar@gmail.com>
2014-11-09 18:04:18 -0800

    Hacky fix with Meteor.setTimeout

--- client/main.js
+++ client/main.js
@@ -1,25 +1,20 @@
-if (Meteor.isClient) {
-	
-	Template.body.rendered = function(){
-		
-		GoogleMaps.init({
-		  sensor: false,
-		  key: 'AIzaSyCJuGapiMUeZt0NHKILcFj5tC10uACna1E',
-		  language: "en",
-		  libraries: "places"
-		}, function() {
-			
-			var autocomplete = new google.maps.places.Autocomplete(
-				(document.getElementById('autocomplete')),{
-					types: ['establishment']
-			});
+if (Meteor.isClient) {	
+	GoogleMaps.init({
+		sensor: false,
+		key: 'AIzaSyCJuGapiMUeZt0NHKILcFj5tC10uACna1E',
+		language: "en",
+		libraries: "places"
+	});
 
-			google.maps.event.addListener(autocomplete, 'place_changed', function() {
-				var place = autocomplete.getPlace();
-				Meteor.call("addPlace", place);
-			});
-
+	Meteor.setTimeout(function(){
+		var autocomplete = new google.maps.places.Autocomplete(
+			(document.getElementById('autocomplete')),{
+				types: ['establishment']
 		});
-		
-	};
+
+		google.maps.event.addListener(autocomplete, 'place_changed', function() {
+			var place = autocomplete.getPlace();
+			Meteor.call("addPlace", place);
+		});
+	}, 1000);
 }

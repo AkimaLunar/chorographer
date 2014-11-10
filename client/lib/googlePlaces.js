45de5cedef493c29b147280dfc524ed451092ca2
Ria Carmin <akimalunar@gmail.com>
2014-11-10 13:23:41 -0800

    google-poogle

--- client/lib/googlePlaces.js
+++ client/lib/googlePlaces.js
@@ -1,3 +1,29 @@
+// var dt = new Date();
+// var completed = false;
+
+// GoogleMaps.init({
+//   sensor: false,
+//   key: 'AIzaSyCJuGapiMUeZt0NHKILcFj5tC10uACna1E',
+//   language: "en",
+//   libraries: "places"},
+
+//   function() {
+//     completed = true;
+//     console.log((new Date()) - dt);
+//       var autocomplete = new google.maps.places.Autocomplete(
+//       (document.getElementById('autocomplete')),{
+//         types: ['establishment']
+//     });
+
+//     google.maps.event.addListener(autocomplete, 'place_changed', function() {
+//       var place = autocomplete.getPlace();
+//       Meteor.call("addPlace", place);
+//     });
+//     }
+// );
+
+//Other way to do it:
+
 GoogleMaps.init({
   sensor: false,
   key: 'AIzaSyCJuGapiMUeZt0NHKILcFj5tC10uACna1E',

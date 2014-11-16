be8b022568582bb038f9bdea79eb23121267c729
Ria Carmin <akimalunar@gmail.com>
2014-11-15 22:46:06 -0800

    Added margin-bottom to the .place-page

--- client/lib/googlePlaces.js
+++ client/lib/googlePlaces.js
@@ -4,13 +4,14 @@ GoogleMaps.init({
   language: "en",
   libraries: "places"
 });
-
+console.log('Google Init');
 Meteor.setTimeout(function(){
+  console.log('var');
   var autocomplete = new google.maps.places.Autocomplete(
     (document.getElementById('autocomplete')),{
       types: ['establishment']
   });
-
+  console.log('event');
   google.maps.event.addListener(autocomplete, 'place_changed', function() {
     var place = autocomplete.getPlace();
     Meteor.call("addPlace", place);
